68edbe613373201cc1d57d6bab08e1cb8e30ee45
Marco Nelissen <marcone@google.com>
2009-10-26 16:19:19 -0700

    Visualizer updates: - consolidate code for waveform/spectrum - use textures instead of overdrawing with different colors - add "analog vu meter" visualization

--- res/raw/waveform.rs
+++ res/raw/waveform.rs
@@ -14,6 +14,8 @@
 
 #pragma version(1)
 #pragma stateVertex(PVBackground)
+#pragma stateRaster(parent)
+#pragma stateFragment(PFBackground)
 
 #define RSID_POINTS 1
 
@@ -26,6 +28,8 @@ int wave2pos = 0;
 int wave2amp= 0;
 int wave3pos = 0;
 int wave3amp= 0;
+int wave4pos = 0;
+int wave4amp= 0;
 
 void dumpState() {
 
@@ -45,30 +49,33 @@ int main(int launchID) {
         if (fadecounter > 0) {
             // fade waveform to 0
             for (i = 0; i < 512; i++) {
-                float val = absf(points[i*4+1]);
+                float val = absf(points[i*8+1]);
                 val = val * fadecounter / FADE_LENGTH;
                 if (val < 2.f) val = 2.f;
-                points[i*4+1] = val;
-                points[i*4+3] = -val;
+                points[i*8+1] = val;
+                points[i*8+5] = -val;
             }
             fadecounter--;
             if (fadecounter == 0) {
                 wave1amp = 0;
                 wave2amp = 0;
                 wave3amp = 0;
+                wave4amp = 0;
             }
         } else {
             // show a number of superimposed moving sinewaves
             float amp1 = sinf(0.007 * wave1amp) * 120;
             float amp2 = sinf(0.023 * wave2amp) * 80;
-            float amp3 = sinf(0.041 * wave3amp) * 20;
+            float amp3 = sinf(0.011 * wave3amp) * 40;
+            float amp4 = sinf(0.031 * wave4amp) * 20;
             for (i = 0; i < 512; i++) {
                 float val = sinf(0.013 * (wave1pos + i)) * amp1
                           + sinf(0.029 * (wave2pos + i)) * amp2;
-                float off = sinf(0.041 * (wave3pos + i)) * amp3;
+                float off = sinf(0.005 * (wave3pos + i)) * amp3
+                          + sinf(0.017 * (wave4pos + i)) * amp4;
                 if (val < 2.f && val > -2.f) val = 2.f;
-                points[i*4+1] = val + off;
-                points[i*4+3] = -val + off;
+                points[i*8+1] = val + off;
+                points[i*8+5] = -val + off;
             }
             wave1pos++;
             wave1amp++;
@@ -76,27 +83,27 @@ int main(int launchID) {
             wave2amp++;
             wave3pos++;
             wave3amp++;
+            wave4pos++;
+            wave4amp++;
         }
     } else {
         fadecounter = FADE_LENGTH;
     }
 
-    // TODO: use a texture instead of overdrawing in different colors
     float mat1[16];
     float yrot = State->yRotation;
     float scale = 0.004f * (1.0f + 2.f * absf(sinf(radf(yrot))));
-    for (i = 0; i < 5; i++) {
-        // Change the model matrix to account for the large model
-        // and to do the necessary rotations.
-        matrixLoadRotate(mat1, yrot, 0.f, 0.f, 1.f);
-        matrixScale(mat1, scale, scale / (i+1), scale);
-        vpLoadModelMatrix(mat1);
 
-        // Draw the visualizer.
-        color(1.0f, 0.25f * i, 0.0f, 1.0f);
-        uploadToBufferObject(NAMED_PointBuffer);
-        drawSimpleMesh(NAMED_CubeMesh);
-    }
+    // Change the model matrix to account for the large model
+    // and to do the necessary rotations.
+    matrixLoadRotate(mat1, yrot, 0.f, 0.f, 1.f);
+    matrixScale(mat1, scale, scale, scale);
+    vpLoadModelMatrix(mat1);
+
+    // Draw the visualizer.
+    uploadToBufferObject(NAMED_PointBuffer);
+    bindTexture(NAMED_PFBackground, 0, NAMED_Tlinetexture);
+    drawSimpleMesh(NAMED_CubeMesh);
 
     return 1;
 }
--- src/com/android/musicvis/vis2/Visualization2RS.java
+++ src/com/android/musicvis/vis2/Visualization2RS.java
@@ -16,211 +16,23 @@
 
 package com.android.musicvis.vis2;
 
+import com.android.musicvis.GenericWaveRS;
 import com.android.musicvis.R;
-import com.android.musicvis.RenderScriptScene;
 
-import android.graphics.Canvas;
-import android.graphics.Rect;
 import android.media.MediaPlayer;
-import android.os.Handler;
-import android.renderscript.Allocation;
-import android.renderscript.Element;
-import android.renderscript.Primitive;
-import android.renderscript.ProgramVertex;
-import android.renderscript.ScriptC;
-import android.renderscript.SimpleMesh;
-import android.renderscript.Type;
-import android.renderscript.Element.Builder;
-import android.util.Log;
-import android.view.SurfaceHolder;
 
-import java.util.TimeZone;
-
-class Visualization2RS extends RenderScriptScene {
-
-    private final Handler mHandler = new Handler();
-    private final Runnable mDrawCube = new Runnable() {
-        public void run() {
-            updateWave();
-        }
-    };
-    private boolean mVisible;
-
-    static class WorldState {
-        public float yRotation;
-        public int idle;
-    }
-    WorldState mWorldState = new WorldState();
-    private Type mStateType;
-    private Allocation mState;
-
-    private SimpleMesh mCubeMesh;
-
-    private Allocation mPointAlloc;
-    private float [] mPointData = new float[512*4];
-
-    private Allocation mLineIdxAlloc;
-    private short [] mIndexData = new short[512*2];
-
-    private ProgramVertex mPVBackground;
-    private ProgramVertex.MatrixAllocation mPVAlloc;
-
-    private short [] mVizData = new short[512];
-
-    private static final int RSID_STATE = 0;
-    private static final int RSID_POINTS = 1;
-    private static final int RSID_LINES = 2;
-    private static final int RSID_PROGRAMVERTEX = 3;
-
+class Visualization2RS extends GenericWaveRS {
 
     Visualization2RS(int width, int height) {
-        super(width, height);
-        mWidth = width;
-        mHeight = height;
-        // the x-coordinates don't change, so set those now
-        int outlen = mPointData.length / 4;
-        int half = outlen / 2;
-        for(int i = 0; i < outlen; i++) {
-            mPointData[i*4]   = i - half;
-            mPointData[i*4+2]   = i - half;
-        }
+        super(width, height, R.drawable.fire);
     }
-
+    
     @Override
-    public void resize(int width, int height) {
-        super.resize(width, height);
-        if (mPVAlloc != null) {
-            Log.i("@@@@@", "resized to " + mWidth + "x" + mHeight);
-            mPVAlloc.setupProjectionNormalized(mWidth, mHeight);
-        }
-    }
-
-    @Override
-    protected ScriptC createScript() {
-
-        // Create a renderscript type from a java class. The specified name doesn't
-        // really matter; the name by which we refer to the object in RenderScript
-        // will be specified later.
-        mStateType = Type.createFromClass(mRS, WorldState.class, 1, "WorldState");
-        // Create an allocation from the type we just created.
-        mState = Allocation.createTyped(mRS, mStateType);
-        // set our java object as the data for the renderscript allocation
-        mWorldState.yRotation = (-0.5f) * 2 * 180 / (float) Math.PI;
-        mState.data(mWorldState);
-
-        /*
-         *  Now put our model in to a form that renderscript can work with:
-         *  - create a buffer of floats that are the coordinates for the points that define the cube
-         *  - create a buffer of integers that are the indices of the points that form lines
-         *  - combine the two in to a mesh
-         */
-
-        // First set up the coordinate system and such
-        ProgramVertex.Builder pvb = new ProgramVertex.Builder(mRS, null, null);
-        mPVBackground = pvb.create();
-        mPVBackground.setName("PVBackground");
-        mPVAlloc = new ProgramVertex.MatrixAllocation(mRS);
-        mPVBackground.bindAllocation(mPVAlloc);
-        mPVAlloc.setupProjectionNormalized(mWidth, mHeight);
-
-        // Start creating the mesh
-        final SimpleMesh.Builder meshBuilder = new SimpleMesh.Builder(mRS);
-
-        // Create the Element for the points
-        Builder elementBuilder = new Builder(mRS);
-        // By specifying a prefix, even an empty one, the members will be accessible
-        // in the renderscript. If we just called addFloatXYZ(), the members would be
-        // unnamed in the renderscript struct definition.
-        elementBuilder.addFloatXY("");
-        final Element vertexElement = elementBuilder.create();
-        final int vertexSlot = meshBuilder.addVertexType(vertexElement, mPointData.length / 2);
-        // Specify the type and number of indices we need. We'll allocate them later.
-        meshBuilder.setIndexType(Element.INDEX_16(mRS), mIndexData.length);
-        // This will be a line mesh
-        meshBuilder.setPrimitive(Primitive.LINE);
-
-        // Create the Allocation for the vertices
-        mCubeMesh = meshBuilder.create();
-        mCubeMesh.setName("CubeMesh");
-        mPointAlloc = mCubeMesh.createVertexAllocation(vertexSlot);
-        mPointAlloc.setName("PointBuffer");
-
-        // Create the Allocation for the indices
-        mLineIdxAlloc = mCubeMesh.createIndexAllocation();
-
-        // Bind the allocations to the mesh
-        mCubeMesh.bindVertexAllocation(mPointAlloc, 0);
-        mCubeMesh.bindIndexAllocation(mLineIdxAlloc);
-
-        /*
-         *  put the vertex and index data in their respective buffers
-         */
-        updateWave();
-        for(int i = 0; i < mIndexData.length; i ++) {
-            mIndexData[i] = (short) i;
-        }
-
-        /*
-         *  upload the vertex and index data
-         */
-        mPointAlloc.data(mPointData);
-        mPointAlloc.uploadToBufferObject();
-        mLineIdxAlloc.data(mIndexData);
-        mLineIdxAlloc.uploadToBufferObject();
-
-        // Time to create the script
-        ScriptC.Builder sb = new ScriptC.Builder(mRS);
-        // Specify the name by which to refer to the WorldState object in the
-        // renderscript.
-        sb.setType(mStateType, "State", RSID_STATE);
-        sb.setType(mCubeMesh.getVertexType(0), "Points", RSID_POINTS);
-        // this crashes when uncommented
-        //sb.setType(mCubeMesh.getIndexType(), "Lines", RSID_LINES);
-        sb.setScript(mResources, R.raw.waveform);
-        sb.setRoot(true);
-
-        ScriptC script = sb.create();
-        script.setClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        script.setTimeZone(TimeZone.getDefault().getID());
-
-        script.bindAllocation(mState, RSID_STATE);
-        script.bindAllocation(mPointAlloc, RSID_POINTS);
-        script.bindAllocation(mLineIdxAlloc, RSID_LINES);
-        script.bindAllocation(mPVAlloc.mAlloc, RSID_PROGRAMVERTEX);
-
-        return script;
-    }
-
-    @Override
-    public void setOffset(float xOffset, float yOffset, int xPixels, int yPixels) {
-        // update our state, then push it to the renderscript
-        mWorldState.yRotation = (xOffset - 0.5f) * 360; // rotate -180 to +180
-        mState.data(mWorldState);
-    }
-
-    @Override
-    public void start() {
-        super.start();
-        mVisible = true;
-        updateWave();
-    }
-
-    @Override
-    public void stop() {
-        super.stop();
-        mVisible = false;
-        updateWave();
-    }
-
-    void updateWave() {
-        mHandler.removeCallbacks(mDrawCube);
-        if (mVisible) {
-            mHandler.postDelayed(mDrawCube, 20);
-        }
+    public void update() {
 
         int len = MediaPlayer.snoop(mVizData, 0);
 
-        int outlen = mPointData.length / 4;
+        int outlen = mPointData.length / 8;
         if (len > outlen) len = outlen;
         
         if (len == 0) {
@@ -237,8 +49,8 @@ class Visualization2RS extends RenderScriptScene {
         // TODO: might be more efficient to push this in to renderscript
         for(int i = 0; i < len; i++) {
             int amp = mVizData[i] / 128;
-            mPointData[i*4+1] = amp;
-            mPointData[i*4+3] = -amp;
+            mPointData[i*8+1] = amp;
+            mPointData[i*8+5] = -amp;
         }
         mPointAlloc.data(mPointData);
 
